235a832c9eeb803342605c26735f76b38feb486c
Samuel Okoye <Samuelokoye3310@gmail.com>
2023-11-02 19:55:09 +0100

    structure

---
+++ app.js
@@ -0,0 +1,18 @@
+// ****** SELECT ITEMS **********
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".form");
+const grocery = document.getElementById("#grocery");
+const submitBtn = document.querySelector(".submit-Btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
+
+// edit option
+
+// ****** EVENT LISTENERS **********
+
+// ****** FUNCTIONS **********
+
+// ****** LOCAL STORAGE **********
+
+// ****** SETUP ITEMS **********
