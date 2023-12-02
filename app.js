3b82c7330c05d3558681197aa9c14a0b55aa7ac9
Samuel Okoye <Samuelokoye3310@gmail.com>
2023-12-02 22:51:35 +0100

    update

--- app.js
+++ app.js
@@ -129,27 +129,41 @@ function removeFromLocalStorage(id){
 // ****** SETUP ITEMS **********
 
 
+
+
+
+
 // delete
 
 import express from "express";
-import morgan from "morgan";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
-function logger(req, res, next){
-  console.log("Request Method: ", req.method);
-  console.log("request URL: ", req.url);
-  next();
-}
-
-app.use(logger);
-
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/submit", (req,res) => {
+  console.log(req.body);
+  return res.send("Thanks for submitting");
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
+
+const fs = require("fs");
+
+fs.writeFile("message.txt", "Hello from NodeJS!", (err) => {
+  if (err) throw err;
+    console.log("Saved!");
+});
